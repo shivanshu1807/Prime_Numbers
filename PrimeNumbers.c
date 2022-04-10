c16426ea606ba63e719a6c159d725287e7071f98
shivanshu1807 <103367759+shivanshu1807@users.noreply.github.com>
2022-04-10 18:43:07 +0530

    Create PrimeNumbers.c

---
+++ PrimeNumbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() 
+{
+   int n,c=0;
+   scanf("%d",&n);
+   for(int i=1;i<=n;i++)
+   {
+       if(n%i==0)
+       {
+           c++;
+       }
+   }
+   if(c==2)
+   {
+       printf("it is prime no.");
+   }
+   else
+   {
+       printf("it is a composite no.");
+   }
+   return 0; 
+}
